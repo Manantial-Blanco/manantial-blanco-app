fc2029a7996222fa4e5feca97a5b8de5e38c944e
gabrielimproving <135665496+gabrielimproving@users.noreply.github.com>
2025-10-06 11:30:47 -0600

    refactor: remove WalletAuthRedirect component from LandingUI

--- components/imported/LandingUI.tsx
+++ components/imported/LandingUI.tsx
@@ -7,7 +7,6 @@ import Image from 'next/image';
 import { Dictionary, Locale } from '@/types';
 import { LanguageDropdown } from '@/components/layout/LanguageDropdown';
 import { WalletUserButton } from '@/components/layout/WalletUserButton';
-import { WalletAuthRedirect } from '@/components/auth/WalletAuthRedirect';
 
 interface LandingUIProps {
   dict: Dictionary;
@@ -103,7 +102,6 @@ export default function LandingUI({ dict, lang, pieces = [] }: LandingUIProps) {
 
   return (
     <div className="min-h-screen w-full flex flex-col bg-background font-sans">
-      <WalletAuthRedirect lang={lang} />
       <style jsx>{`
         .nav-link {
           font-family: 'SF Compact Display', sans-serif;
